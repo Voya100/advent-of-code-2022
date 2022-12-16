ab7e20075f92c87eafdc7428b36d43699e7f3a0a
Voya <voya@voyacode.com>
2022-12-16 22:07:28 +0200

    Optimise day 16 part 1 solution
    Memorise old "states"
    
    Performance change: ~820 s -> ~6 s

--- src/day16.ts
+++ src/day16.ts
@@ -5,12 +5,25 @@ interface Valve {
   flowRate: number;
   neighborIds: string[];
   neighbors: Valve[];
+  opened: boolean;
+}
+
+interface CachedResult {
+  resultsByTimeRemaining: Record<number, number>;
+  highestTimeRemaining: number;
 }
 
 export function part1(input: string) {
   const valves = parseInput(input);
   const startingValve = valves.find((valve) => valve.id === 'AA');
-  const result = findMaxValvePressure(startingValve, new Set(), new Set(), 30);
+  const cachedValues: Record<string, CachedResult> = {};
+  const result = findMaxValvePressure(
+    startingValve,
+    valves,
+    new Set(),
+    30,
+    cachedValues
+  );
   return result;
 }
 
@@ -39,19 +52,32 @@ function parseValve(row: string): Valve {
     flowRate: +rate,
     neighborIds: tunnels.split(', '),
     neighbors: [],
+    // No reason to open valves with no flow, so can be marked as open
+    opened: +rate === 0,
   };
 }
 
 function findMaxValvePressure(
   valve: Valve,
-  openedValves: Set<Valve>,
+  valves: Valve[],
   skippedValves: Set<Valve>,
-  timeRemaining: number
+  timeRemaining: number,
+  cachedValues: Record<string, CachedResult>
 ): number {
+  iterCount++;
   if (timeRemaining <= 1) {
     return 0;
   } else if (timeRemaining === 2) {
-    return openedValves.has(valve) ? 0 : valve.flowRate;
+    return valve.opened ? 0 : valve.flowRate;
+  }
+  const cacheKey = getCacheKey(valve, valves);
+  const cachedValue = cachedValues[cacheKey];
+  if (cachedValue && cachedValue.resultsByTimeRemaining[timeRemaining]) {
+    // Already processed this state => Return cached result
+    return cachedValue.resultsByTimeRemaining[timeRemaining];
+  } else if (cachedValue && cachedValue.highestTimeRemaining > timeRemaining) {
+    // Has reached this state before in quicker time => not the highest result
+    return 0;
   }
   const nextValves = valve.neighbors.filter(
     (neighbor) => !skippedValves.has(neighbor)
@@ -61,28 +87,48 @@ function findMaxValvePressure(
   const pressureValues1 = nextValves.map((neighbor) =>
     findMaxValvePressure(
       neighbor,
-      openedValves,
+      valves,
       skippedValves,
-      timeRemaining - 1
+      timeRemaining - 1,
+      cachedValues
     )
   );
   skippedValves.delete(valve);
   let pressureValues2: number[] = [];
-  if (valve.flowRate && !openedValves.has(valve)) {
+  if (!valve.opened) {
     // Scenario 2: Open current valve
-    openedValves.add(valve);
+    valve.opened = true;
     pressureValues2 = valve.neighbors.map(
       (neighbor) =>
         // Reset skipped valves after opening the valve, since it's possible to return
         findMaxValvePressure(
           neighbor,
-          openedValves,
+          valves,
           new Set(),
-          timeRemaining - 2
+          timeRemaining - 2,
+          cachedValues
         ) +
         valve.flowRate * (timeRemaining - 1)
     );
-    openedValves.delete(valve);
+    valve.opened = false;
   }
-  return Math.max(...pressureValues1, ...pressureValues2);
+  const maxPressure = Math.max(...pressureValues1, ...pressureValues2);
+  if (cachedValue) {
+    cachedValue.resultsByTimeRemaining[timeRemaining] = maxPressure;
+    if (cachedValue.highestTimeRemaining < timeRemaining) {
+      cachedValue.highestTimeRemaining = timeRemaining;
+    }
+  } else {
+    cachedValues[cacheKey] = {
+      resultsByTimeRemaining: {
+        [timeRemaining]: maxPressure,
+      },
+      highestTimeRemaining: timeRemaining,
+    };
+  }
+  return maxPressure;
+}
+
+function getCacheKey(valve: Valve, valves: Valve[]) {
+  return valve.id + '_' + valves.map((valve) => +valve.opened).join('');
 }
